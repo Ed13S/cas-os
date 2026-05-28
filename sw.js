6ecefee9a98ce5bdf56976da616f942478633cf3
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 21:39:42 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,27 @@
-self.addEventListener("install", e => {
-
+self.addEventListener("install", e=>{
   e.waitUntil(
-
-    caches.open("cas-os").then(cache => {
-
+    caches.open("cas_os").then(cache=>{
       return cache.addAll([
         "./",
         "./index.html",
-        "./style.css",
         "./app.js",
         "./cas-engine.js",
         "./steps.js",
+        "./graph.js",
         "./physics.js",
         "./chemistry.js",
-        "./graph.js",
         "./handwriting.js",
-        "./ai-mini.js"
+        "./ai-mini.js",
+        "./voice.js",
+        "./camera.js",
+        "./style.css"
       ]);
-
     })
-
   );
-
 });
 
-self.addEventListener("fetch", e => {
-
+self.addEventListener("fetch", e=>{
   e.respondWith(
-
-    caches.match(e.request).then(r => r || fetch(e.request))
-
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
-
 });
