d0d69e479091dc31de5b9941fdd19bdc688621ce
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 21:20:47 +1000

    Update cas-engine.js

--- cas-engine.js
+++ cas-engine.js
@@ -1,23 +1,21 @@
 function CAS(input){
   try{
+
     input = input.trim();
 
     if(input.startsWith("solve")){
       let eq = input.replace("solve","").trim();
-      return "Solve Steps:\n1) Move all terms to one side\n2) Factor or use formula\n3) Solve roots\n\nResult:\n" +
-        Algebrite.run(`roots(${eq})`);
+      return "Solve:\n" + Algebrite.run(`roots(${eq})`);
     }
 
     if(input.startsWith("diff")){
       let e = input.replace("diff","").trim();
-      return "Derivative Steps:\n1) Apply power rule\n2) Reduce exponent\n\nResult:\n" +
-        Algebrite.run(`d(${e})`);
+      return "Derivative:\n" + Algebrite.run(`d(${e})`);
     }
 
     if(input.startsWith("int")){
       let e = input.replace("int","").trim();
-      return "Integral Steps:\n1) Increase power\n2) Divide\n3) +C\n\nResult:\n" +
-        Algebrite.run(`integral(${e})`);
+      return "Integral:\n" + Algebrite.run(`integral(${e})`);
     }
 
     if(input.startsWith("factor")){
