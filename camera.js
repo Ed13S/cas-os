98f8a3fd4f9733f9bf0590efb6e0c62170e83bb6
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 21:39:01 +1000

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,20 @@
+async function readCamera(){
+  const file=document.getElementById("cameraInput").files[0];
+  if(!file) return;
 
+  const img=await createImageBitmap(file);
+
+  const worker=Tesseract.createWorker();
+
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+
+  const { data:{text} } = await worker.recognize(img);
+
+  await worker.terminate();
+
+  document.getElementById("cameraOut").textContent=text;
+  document.getElementById("input").value=text;
+  run();
+}
