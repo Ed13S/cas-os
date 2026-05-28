acc243735d733f9ef12a37b3de82aa9e912a05c2
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 20:58:01 +1000

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,1 +1,10 @@
-
+function plot(expr){
+  let x=[], y=[];
+  for(let i=-50;i<=50;i++){
+    x.push(i);
+    try{y.push(math.evaluate(expr,{x:i}));}catch{y.push(null);}
+  }
+  Plotly.newPlot("graph",[{
+    x,y,type:"scatter",line:{color:"#00ff88"}
+  }],{paper_bgcolor:"#000", plot_bgcolor:"#000"});
+}
