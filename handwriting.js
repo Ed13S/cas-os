f9eb02839fb8b499502eb43af22d3c55a6786b8b
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 21:37:54 +1000

    Update handwriting.js

--- handwriting.js
+++ handwriting.js
@@ -1,24 +1,24 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext("2d");
 
-let drawing = false;
+let drawing=false;
 
 canvas.addEventListener("mousedown",(e)=>{
-  drawing = true;
+  drawing=true;
   ctx.beginPath();
   ctx.moveTo(e.offsetX,e.offsetY);
 });
 
-canvas.addEventListener("mouseup",()=>{
-  drawing = false;
-});
+canvas.addEventListener("mouseup",()=>drawing=false);
 
 canvas.addEventListener("mousemove",(e)=>{
   if(!drawing) return;
-
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "#00ff88";
-
+  ctx.lineWidth=2;
+  ctx.strokeStyle="#00ff00";
   ctx.lineTo(e.offsetX,e.offsetY);
   ctx.stroke();
 });
+
+function clearCanvas(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+}
