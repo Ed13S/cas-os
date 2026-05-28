ddb41a4d07bc4f020332ea677f6c2746506f6368
Eddie Singh <121286867+Ed13S@users.noreply.github.com>
2026-05-28 21:35:49 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 function run(){
-
   const input = document.getElementById("input").value;
 
-  let result = CAS(input);
+  const result = CAS(input);
 
   document.getElementById("output").textContent =
-    result + "\n\n" + explain(input);
+    "CAS_OS OUTPUT:\n" + result + "\n\n" + explain(input);
 
   if(input.startsWith("plot"))
     plot(input.replace("plot","").trim());
+
+  if(input.includes("physics"))
+    document.getElementById("output").textContent += "\n\n" + physics(input);
+
+  if(input.includes("chemistry"))
+    document.getElementById("output").textContent += "\n\n" + chemistry(input);
 }
